b24d9d13081c4da57f12c66dfcf771ef3152309f
Сергей Ананьин <s.ananin@megabonus.ru>
2023-04-21 09:59:30 +0500

    init

---
+++ src/admin/Module.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace sitis\googleindexer\admin;
+
+use luya\admin\components\AdminMenuBuilder;
+use Yii;
+use yii\helpers\ArrayHelper;
+
+/**
+ * Menu Admin Module.
+ *
+ * @author Mikhail Chemezov <michlenanosoft@gmail.com>
+ * @since 1.0.0
+ */
+class Module extends \luya\admin\base\Module
+{
+
+}
